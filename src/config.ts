608d9f2bc0212e82398f9513784ea4fffe2b8ede
Samuel Pandohan Terampil Gultom <adalgisoabercio2001@gmail.com>
2023-12-28 01:27:45 +0700

    feat: Initial commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'adalgisoabercio19 is here 👋';
+export const SITE_DESCRIPTION = 'My specialties include Critical Thinking, Data-Driven Decision Making, Data Analytics, Machine Learning Algorithms, several Back-End methods, and currently learn ML-to-Back-End Implementations optimally.';
